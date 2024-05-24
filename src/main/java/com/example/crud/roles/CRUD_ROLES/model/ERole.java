4b386ecb463f3b65563d5ba94813ace4f217ddb6
Samuel Alexander Perdomo <samuelalexp21@gmail.com>
2024-05-24 09:30:17 -0500

    ultimas configuraciones respecto JWT. Se soluciono el error 500 respecto a /auth/generateToken pero sale error 401 respecto a /auth/hello

---
+++ src/main/java/com/example/crud/roles/CRUD_ROLES/model/ERole.java
@@ -0,0 +1,8 @@
+package com.example.crud.roles.CRUD_ROLES.model;
+
+public enum ERole {
+
+    ROLE_USER,
+    ROLE_ADMIN
+
+}
--- src/main/java/com/example/crud/roles/CRUD_ROLES/security/JwtUtils.java
+++ src/main/java/com/example/crud/roles/CRUD_ROLES/security/JwtUtils.java
@@ -29,7 +29,8 @@ import java.util.function.Function;
 @Component
 public class JwtUtils {
 
-    private  static  String SECRET_KEY = "el_samu";
+    private static final String SECRET = "9D0EB6B1C2E1FAD0F53A248F6C3B5E4E2F6D8G3H1I0J7K4L1M9N2O3P5Q0R7S9T1U4V2W6X0Y3Z"; ;
+
 
     public String generateToken(String userName) {
         Map<String, Object> claims = new HashMap<>();
@@ -47,7 +48,7 @@ public class JwtUtils {
     }
 
     private Key getSignKey() {
-        byte[] keyBytes= Decoders.BASE64.decode(SECRET_KEY);
+        byte[] keyBytes= Decoders.BASE64.decode(SECRET);
         return Keys.hmacShaKeyFor(keyBytes);
     }
 
